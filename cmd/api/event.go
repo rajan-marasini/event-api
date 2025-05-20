21c18c31314527e9c80cc812dfa539ebaee1a680
Rajan Marasini <official.rajan2060@gmail.com>
2025-05-20 13:46:03 +0545

    created api endpoints for events

--- cmd/api/event.go
+++ cmd/api/event.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rajan-marasini/event-api/internal/models"
@@ -21,4 +22,82 @@ func (app *application) createEvent(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusCreated, event)
+}
+
+func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invaild event id"})
+		return
+	}
+	event, err := app.models.Event.Get(id)
+
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+	c.JSON(http.StatusOK, event)
+}
+
+func (app *application) getAllEvent(c *gin.Context) {
+	events, err := app.models.Events.GetAll()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve events"})
+		return
+	}
+
+	c.JSON(http.StatusOK, events)
+}
+
+func (app *application) updateEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event Id"})
+		return
+	}
+	existingEvent, err := app.models.Event.Get(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+
+	if existingEvent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	updatedEvent := &models.Event{}
+	if err := c.ShouldBindJSON(&updatedEvent); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.ID = id
+	if err := app.models.Event.Update(updatedEvent); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update event"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedEvent)
+}
+
+func (app *application) deleteEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event id"})
+		return
+	}
+
+	if err := app.models.Event.Delete(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete event"})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
 }
--- cmd/api/route.go
+++ cmd/api/route.go
@@ -12,10 +12,10 @@ func (app *application) route() http.Handler {
 	v1 := g.Group("/api/v1")
 	{
 		v1.POST("/event", app.createEvent)
-		// v1.GET("/events", app.getAllEvent)
-		// v1.GET("/event/:id", app.getEvent)
-		// v1.PUT("/event/:id", app.updateEvent)
-		// v1.DELETE("/event/:id", app.deleteEvent)
+		v1.GET("/events", app.getAllEvent)
+		v1.GET("/event/:id", app.getEvent)
+		v1.PUT("/event/:id", app.updateEvent)
+		v1.DELETE("/event/:id", app.deleteEvent)
 	}
 
 	return g
